ce4a72774144900b0f9111711118ad4d4f8e793d
Lena Sokolova <sokolhelena94@gmail.com>
2022-11-09 11:09:50 +0100

    feat: add ThreadList comp

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import styled from 'styled-components';
 import Sidebar from './components/Sidebar';
 import Navbar from './components/Navbar';
+import ThreadsList from './components/ThreadsList';
 
 function App() {
   return (
     <AppBody>
       {/* <h1>Let's build Prodcut Feedbck App</h1>s */}
       <Sidebar />
-      <Navbar />
+      <Main>
+        <Navbar />
+        <ThreadsList />
+
+      </Main>
       {/*Feed Threads*/}
       {/*Thread List*/}
 
@@ -27,4 +32,9 @@ export default App;
 const AppBody = styled.div`
   display: flex;
   height: 100vh;
+`;
+
+
+const Main = styled.div`
+  flex: 0.7;
 `;
