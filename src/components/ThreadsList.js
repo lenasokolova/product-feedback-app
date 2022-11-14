ec740bc950a73b1469c67101da4ae92ecc580b4a
Lena Sokolova <sokolhelena94@gmail.com>
2022-11-14 14:28:11 +0100

    refactor: render feedback from db

--- src/components/ThreadsList.js
+++ src/components/ThreadsList.js
@@ -1,15 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Thread from './Thread';
 
+import { db } from '../firebase';
+
 const ThreadsList = () => {
+
+    const [loading, setLoading] = useState(true);
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const getPostsFromFirebase = [];
+        const feedbacks = db.collection('feedbacks').onSnapshot((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                getPostsFromFirebase.push({
+                    ...doc.data(),
+                    key: doc.id,
+                })
+                setPosts(getPostsFromFirebase);
+                setLoading(false);
+            })
+        })
+
+        return () => feedbacks();
+    }, [])
+
+
+    console.log(posts)
+
+    if (loading) {
+        return <h1>Loading firebase data...</h1>
+    }
+
+    const renderedFeedbacks = posts.map(post => {
+
+        return (
+            <Thread
+                title={post.title}
+                category={post.category}
+                detail={post.detail}
+                commentsCount={post.commentsCount}
+                upVotesCount={post.upVotesCount}
+                key={post.key}
+            />
+
+        )
+
+
+    })
+
+
     return (
         <ThreadListContainer>
-            <Thread />
-            <Thread />
-            <Thread />
-            <Thread />
-            <Thread />
+            {renderedFeedbacks}
+
+
 
         </ThreadListContainer>
     )
