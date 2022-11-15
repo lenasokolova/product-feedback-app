8b18102077fc6e6a61e3ec9713caeca59fe7b936
Lena Sokolova <sokolhelena94@gmail.com>
2022-11-15 15:02:22 +0100

    refactor: change the import file path

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import feedbackReducer from '../features/feedbackSlice';
+import feedbackReducer from '../features/feedbacks/feedbackSlice';
 
 export const store = configureStore({
   reducer: {
