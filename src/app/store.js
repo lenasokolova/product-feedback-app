1d0a37dac9ae69c929f30c42dabff9e18c006398
Lena Sokolova <sokolhelena94@gmail.com>
2022-11-15 15:07:45 +0100

    refactor: add userReducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import feedbackReducer from '../features/feedbacks/feedbackSlice';
+import userReducer from '../features/users/userSlice';
+
 
 export const store = configureStore({
   reducer: {
     feedback: feedbackReducer,
+    user: userReducer,
   },
 });
