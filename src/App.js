cdc73e75a6672f16e219d7e2a60e24f6e0357b05
Lena Sokolova <sokolhelena94@gmail.com>
2022-11-15 14:14:38 +0100

    delete unused file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import styled from 'styled-components';
-import Sidebar from './components/Sidebar';
-import Navbar from './components/Navbar';
-import ThreadsList from './components/ThreadsList';
-import ThreadPage from './components/ThreadPage';
 import CreateNewFeedbackPage from './pages/CreateNewFeedbackPage';
 
 import { Routes, Route } from "react-router-dom";
 import DashboardPage from './pages/DashboardPage';
+import SingleThreadPage from './pages/SingleThreadPage';
 
 const App = () => {
   return (
 
     <Routes>
-      <Route path='/' element={<DashboardPage />} />
-      <Route path='/add-feedback' element={<CreateNewFeedbackPage />} />
+
+      <Route path='/' element={<DashboardPage />}>
+
+      </Route>
+      <Route path='feedback'>
+        <Route index element={<CreateNewFeedbackPage />} />
+
+        <Route path=':feedbackId' element={<SingleThreadPage />} />
+      </Route>
 
     </Routes>
   );
