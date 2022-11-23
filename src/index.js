898ee0a0694f229273f6eff377511d27198ac5df
Lena Sokolova <sokolhelena94@gmail.com>
2022-11-23 16:38:32 +0100

    refactor: change the path for store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+import store from './redux/store';
 import { BrowserRouter } from "react-router-dom";
 import App from './App';
 import './index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
+
 
 root.render(
   <React.StrictMode>
@@ -18,3 +19,16 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
+// const container = document.getElementById('root');
+// const root = createRoot(container);
+
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <Provider store={store}>
+//         <App />
+//       </Provider>
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
