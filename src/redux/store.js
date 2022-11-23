17ea3daeffc36621a8bb5224e903dc4faf86b89b
Lena Sokolova <sokolhelena94@gmail.com>
2022-11-23 16:41:10 +0100

    feat: create store

---
+++ src/redux/store.js
@@ -0,0 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+import rootReducer from './root-reducer';
+
+// const middleware = [thunk];
+
+// if (process.env.NODE_ENV === 'development') {
+//     middleware.push(logger);
+// }
+
+const store = configureStore({
+    reducer: rootReducer,
+    middleware: [thunk, logger],
+});
+
+export default store;
