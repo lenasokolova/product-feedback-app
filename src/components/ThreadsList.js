891561bfbbe226346d5e83176c01b071cd546fe4
Lena Sokolova <sokolhelena94@gmail.com>
2022-11-23 16:39:07 +0100

    refactor: change the logic for get data from firebase

--- src/components/ThreadsList.js
+++ src/components/ThreadsList.js
@@ -1,75 +1,134 @@
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import ThreadExcerpt from './ThreadExcerpt';
+// import styled from 'styled-components';
+// import ThreadExcerpt from './ThreadExcerpt';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { db } from '../firebase';
+import { getFeedbacks } from './../redux/actions';
 
 const ThreadsList = () => {
+    // const [loading, setLoading] = useState(true);
+    const [data, setData] = useState({});
+    console.log("data", data);
 
-    const [loading, setLoading] = useState(true);
-    const [feedbacksAll, setFeedbacksAll] = useState([]);
+    const state = useSelector(state => state.data);
+    console.log("state =>", state);
+
+    let dispatch = useDispatch();
 
     useEffect(() => {
-        const getFeedbacksFromFirebase = [];
-        const feedbacks = db.collection('feedbacks').onSnapshot((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                getFeedbacksFromFirebase.push({
-                    data: doc.data(),
-                    key: doc.id,
-                })
-                setFeedbacksAll(getFeedbacksFromFirebase);
-                setLoading(false);
-            })
-        })
-
-        return () => feedbacks();
+        dispatch(getFeedbacks());
     }, [])
 
 
-    console.log(feedbacksAll)
+    // console.log(feedbacksAll)
 
-    if (loading) {
-        return <h1>Loading firebase data...</h1>
-    }
+    // if (loading) {
+    //     return <h1>Loading firebase data...</h1>
+    // }
 
-    const renderedFeedbacks = feedbacksAll.map(({
-        key,
-        data: {
-            id,
-            title,
-            category,
-            detail,
-            commentsCount,
-            upVotesCount
-        } }) => {
+    // const renderedFeedbacks = feedbacksAll.map(({
+    //     key,
+    //     data: {
+    //         id,
+    //         title,
+    //         category,
+    //         detail,
+    //         commentsCount,
+    //         upVotesCount
+    //     } }) => {
 
-        return (
-            <ThreadExcerpt
-                id={id}
-                key={key}
-                title={title}
-                category={category}
-                detail={detail}
-                commentsCount={commentsCount}
-                upVotesCount={upVotesCount}
-            />
+    //     return (
+    //         <ThreadExcerpt
+    //             id={id}
+    //             key={key}
+    //             title={title}
+    //             category={category}
+    //             detail={detail}
+    //             commentsCount={commentsCount}
+    //             upVotesCount={upVotesCount}
+    //         />
 
-        )
-
-
-    })
+    //     )
 
 
-    return (
-        <ThreadListContainer>
-            {renderedFeedbacks}
-        </ThreadListContainer>
-    )
+    // })
+
+
+    // return (
+    //     <ThreadListContainer>
+    //         {renderedFeedbacks}
+    //     </ThreadListContainer>
+    // )
+
+
 }
 
 export default ThreadsList
 
 
-const ThreadListContainer = styled.div`
-    height: 100vh;
-`;
+// const ThreadListContainer = styled.div`
+//     height: 100vh;
+// `;
+
+
+
+// const [loading, setLoading] = useState(true);
+    // const [feedbacksAll, setFeedbacksAll] = useState([]);
+
+    // useEffect(() => {
+    //     const getFeedbacksFromFirebase = [];
+    //     const feedbacks = db.collection('feedbacks').onSnapshot((querySnapshot) => {
+    //         querySnapshot.forEach((doc) => {
+    //             getFeedbacksFromFirebase.push({
+    //                 data: doc.data(),
+    //                 key: doc.id,
+    //             })
+    //             setFeedbacksAll(getFeedbacksFromFirebase);
+    //             setLoading(false);
+    //         })
+    //     })
+
+    //     return () => feedbacks();
+    // }, [])
+
+
+    // console.log(feedbacksAll)
+
+    // if (loading) {
+    //     return <h1>Loading firebase data...</h1>
+    // }
+
+    // const renderedFeedbacks = feedbacksAll.map(({
+    //     key,
+    //     data: {
+    //         id,
+    //         title,
+    //         category,
+    //         detail,
+    //         commentsCount,
+    //         upVotesCount
+    //     } }) => {
+
+    //     return (
+    //         <ThreadExcerpt
+    //             id={id}
+    //             key={key}
+    //             title={title}
+    //             category={category}
+    //             detail={detail}
+    //             commentsCount={commentsCount}
+    //             upVotesCount={upVotesCount}
+    //         />
+
+    //     )
+
+
+    // })
+
+
+    // return (
+    //     <ThreadListContainer>
+    //         {renderedFeedbacks}
+    //     </ThreadListContainer>
+    // )
