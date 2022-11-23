c66d467e26215f2765d2f74b72afae4783dc1414
Lena Sokolova <sokolhelena94@gmail.com>
2022-11-23 16:38:00 +0100

    add databaseURL

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,12 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
+import { getDatabase, ref } from 'firebase/database';
 
 const firebaseConfig = {
     apiKey: "AIzaSyCjVIkpex_q4jLyKqsKFkWd7gLVRzG24oI",
     authDomain: "product-feedback-app-20ece.firebaseapp.com",
+    databaseURL: "https://product-feedback-app-20ece-default-rtdb.firebaseio.com",
     projectId: "product-feedback-app-20ece",
     storageBucket: "product-feedback-app-20ece.appspot.com",
     messagingSenderId: "120698199881",
@@ -15,5 +17,7 @@ const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
+
+export default ref(getDatabase())
 
 export { auth, provider, db };
