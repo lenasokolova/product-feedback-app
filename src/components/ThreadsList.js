a09088f95d19c75f017bd480c8eddce5241b4a39
Lena Sokolova <sokolhelena94@gmail.com>
2022-11-24 14:27:01 +0100

    refactor: add the renderedFeedbacks retrieved from db

--- src/components/ThreadsList.js
+++ src/components/ThreadsList.js
@@ -1,18 +1,13 @@
-import React, { useState, useEffect } from 'react';
-// import styled from 'styled-components';
-// import ThreadExcerpt from './ThreadExcerpt';
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import ThreadExcerpt from './ThreadExcerpt';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { db } from '../firebase';
 import { getFeedbacks } from './../redux/actions';
 
 const ThreadsList = () => {
-    // const [loading, setLoading] = useState(true);
-    const [data, setData] = useState({});
-    console.log("data", data);
 
-    const state = useSelector(state => state.data);
-    console.log("state =>", state);
+    const { feedbacks: data } = useSelector(state => state.data);
 
     let dispatch = useDispatch();
 
@@ -20,115 +15,30 @@ const ThreadsList = () => {
         dispatch(getFeedbacks());
     }, [])
 
+    const renderedFeedbacks = Object.keys(data).map((id) => {
+        return (
+            <ThreadExcerpt
+                id={data[id].id}
+                key={data[id].id}
+                title={data[id].title}
+                category={data[id].category}
+                detail={data[id].detail}
+                commentsCount={data[id].commentsCount}
+                upVotesCount={data[id].upVotesCount}
+            />
+        )
+    })
 
-    // console.log(feedbacksAll)
-
-    // if (loading) {
-    //     return <h1>Loading firebase data...</h1>
-    // }
-
-    // const renderedFeedbacks = feedbacksAll.map(({
-    //     key,
-    //     data: {
-    //         id,
-    //         title,
-    //         category,
-    //         detail,
-    //         commentsCount,
-    //         upVotesCount
-    //     } }) => {
-
-    //     return (
-    //         <ThreadExcerpt
-    //             id={id}
-    //             key={key}
-    //             title={title}
-    //             category={category}
-    //             detail={detail}
-    //             commentsCount={commentsCount}
-    //             upVotesCount={upVotesCount}
-    //         />
-
-    //     )
-
-
-    // })
-
-
-    // return (
-    //     <ThreadListContainer>
-    //         {renderedFeedbacks}
-    //     </ThreadListContainer>
-    // )
-
-
+    return (
+        <ThreadListContainer>
+            {renderedFeedbacks}
+        </ThreadListContainer>
+    )
 }
 
 export default ThreadsList
 
 
-// const ThreadListContainer = styled.div`
-//     height: 100vh;
-// `;
-
-
-
-// const [loading, setLoading] = useState(true);
-    // const [feedbacksAll, setFeedbacksAll] = useState([]);
-
-    // useEffect(() => {
-    //     const getFeedbacksFromFirebase = [];
-    //     const feedbacks = db.collection('feedbacks').onSnapshot((querySnapshot) => {
-    //         querySnapshot.forEach((doc) => {
-    //             getFeedbacksFromFirebase.push({
-    //                 data: doc.data(),
-    //                 key: doc.id,
-    //             })
-    //             setFeedbacksAll(getFeedbacksFromFirebase);
-    //             setLoading(false);
-    //         })
-    //     })
-
-    //     return () => feedbacks();
-    // }, [])
-
-
-    // console.log(feedbacksAll)
-
-    // if (loading) {
-    //     return <h1>Loading firebase data...</h1>
-    // }
-
-    // const renderedFeedbacks = feedbacksAll.map(({
-    //     key,
-    //     data: {
-    //         id,
-    //         title,
-    //         category,
-    //         detail,
-    //         commentsCount,
-    //         upVotesCount
-    //     } }) => {
-
-    //     return (
-    //         <ThreadExcerpt
-    //             id={id}
-    //             key={key}
-    //             title={title}
-    //             category={category}
-    //             detail={detail}
-    //             commentsCount={commentsCount}
-    //             upVotesCount={upVotesCount}
-    //         />
-
-    //     )
-
-
-    // })
-
-
-    // return (
-    //     <ThreadListContainer>
-    //         {renderedFeedbacks}
-    //     </ThreadListContainer>
-    // )
+const ThreadListContainer = styled.div`
+    height: 100vh;
+`;
