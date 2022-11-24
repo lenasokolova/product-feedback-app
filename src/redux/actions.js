a3f839284437caf203ed331b3ec09f3ebc6c3081
Lena Sokolova <sokolhelena94@gmail.com>
2022-11-24 14:09:31 +0100

    fix: solved the problem with undefined data retrieved from the db

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,6 +1,6 @@
 import * as types from './actionTypes'
 import { db } from '../firebase';
-import dbRef from '../firebase';
+// import dbRef from '../firebase';
 import { collection, getDocs } from "firebase/firestore";
 
 
@@ -21,97 +21,24 @@ const getFeedbacksFail = () => ({
 
 
 export const getFeedbacks = () => {
-    return async function (dispatch) {
-        dispatch(getFeedbacksStart());
+    return function (dispatch) {
+        dispatch(getFeedbacksStart())
 
+        const getData = async () => {
+            try {
+                const querySnapshot = await getDocs(collection(db, "feedbacks"));
+                const feedbacks = querySnapshot.docs.map((doc) => ({
+                    ...doc.data(),
+                    id: doc.id
+                }))
+                dispatch(getFeedbacksSussess(feedbacks));
 
+            } catch (error) {
+                dispatch(getFeedbacksFail(error))
+            }
 
-        try {
-            const querySnapshot = await getDocs(collection(db, "feedbacks"));
-            // querySnapshot.forEach((doc) => {
-            //     console.log(doc.id, " => ", doc.data())
-            // });
-
-            // const feedbacks = querySnapshot.forEach((doc) => {
-            //     console.log(doc.id, " => ", doc.data())
-            // })
-            dispatch(getFeedbacksSussess(querySnapshot))
-
-            // setData({});
-
-        } catch (error) {
-            dispatch(getFeedbacksFail(error))
         }
-
-
-
-
-        // import { collection, getDocs } from "firebase/firestore";
-
-        // const querySnapshot = await getDocs(collection(db, "cities"));
-        // querySnapshot.forEach((doc) => {
-        // // doc.data() is never undefined for query doc snapshots
-        // console.log(doc.id, " => ", doc.data());
-        // });
-
-        // const getFeedbacksFromFirebase = [];
-        //     const feedbacks = db.collection('feedbacks').onSnapshot((querySnapshot) => {
-        //         querySnapshot.forEach((doc) => {
-        //             getFeedbacksFromFirebase.push({
-        //                 data: doc.data(),
-        //                 key: doc.id,
-        //             })
-        //             setFeedbacksAll(getFeedbacksFromFirebase);
-        //             setLoading(false);
-        //         })
-        //     })
-
-        //     return () => feedbacks();
-
-
-
-
-
-        // dbRef.child("feedbacks").on("value", (snapshot) => {
-        //     try {
-        //         if (snapshot.val() !== null) {
-        //             dispatch(getFeedbacksSussess(snapshot.val()));
-        //         } else {
-        //             // setData({});
-        //         }
-        //     } catch (error) {
-        //         dispatch(getFeedbacksFail(error))
-        //     }
-        // })
-
-
+        getData();
     }
 }
 
-
-// import { getDatabase, ref, child, get } from "firebase/database";
-// import { useDispatch } from 'react-redux';
-
-// const dbRef = ref(getDatabase());
-// get(child(dbRef, `users/${userId}`)).then((snapshot) => {
-//   if (snapshot.exists()) {
-//     console.log(snapshot.val());
-//   } else {
-//     console.log("No data available");
-//   }
-// }).catch((error) => {
-//   console.error(error);
-// });
-
-
-
-// const dbRef = firebase.database().ref();
-// dbRef.child("users").child(userId).get().then((snapshot) => {
-//   if (snapshot.exists()) {
-//     console.log(snapshot.val());
-//   } else {
-//     console.log("No data available");
-//   }
-// }).catch((error) => {
-//   console.error(error);
-// });
