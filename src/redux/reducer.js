1dd46fa888a77b6b18a91b7e14aff27725f18e5f
Lena Sokolova <sokolhelena94@gmail.com>
2022-11-26 11:19:48 +0100

    feat: add edit feedbaks action types

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -27,42 +27,26 @@ const feedbackReducer = (state = initialState, action) => {
             }
         case types.DELETE_FEEDBACK_START:
         case types.ADD_FEEDBACK_START:
+        case types.EDIT_FEEDBACK_START:
             return {
                 ...state,
                 loading: true
             }
         case types.DELETE_FEEDBACK_SUCCESS:
         case types.ADD_FEEDBACK_SUCCESS:
-            return {
-                ...state,
-                loading: false,
-
-            }
-        case types.DELETE_FEEDBACK_FAIL:
-        case types.ADD_FEEDBACK_FAIL:
-            return {
-                ...state,
-                loading: false,
-                error: action.payload,
-            }
-        case types.EDIT_FEEDBACK_START:
-            return {
-                ...state,
-                loading: true
-            }
         case types.EDIT_FEEDBACK_SUCCESS:
             return {
                 ...state,
                 loading: false,
-
             }
+        case types.DELETE_FEEDBACK_FAIL:
+        case types.ADD_FEEDBACK_FAIL:
         case types.EDIT_FEEDBACK_FAIL:
             return {
                 ...state,
                 loading: false,
                 error: action.payload,
             }
-
         default:
             return state;
     }
