a75d8698ac1570605b656f288f0f4d3f170ae74c
Lena Sokolova <sokolhelena94@gmail.com>
2022-11-30 16:58:49 +0100

    refactor: add middleware for using logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,20 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { applyMiddleware, configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
-import rootReducer from './root-reducer';
+import rootReducer from './rootReducer';
 
-// const middleware = [thunk];
+const middleware = [thunk];
 
-// if (process.env.NODE_ENV === 'development') {
-//     middleware.push(logger);
-// }
+if (process.env.NODE_ENV === 'development') {
+    middleware.push(logger);
+}
 
 const store = configureStore({
     reducer: rootReducer,
     middleware: [thunk, logger],
 });
+
+// const store = configureStore(rootReducer, applyMiddleware(...middleware));
 
 export default store;
