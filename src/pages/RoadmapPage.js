eaf953780f0e3f3f8fd05ac6c8298b946a458500
Lena Sokolova <sokolhelena94@gmail.com>
2022-11-30 16:56:23 +0100

    feat: create Roadmap page

---
+++ src/pages/RoadmapPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavabarRoadmap from '../components/NavabarRoadmap'
+import RoadmapFeedCard from '../components/RoadmapFeedCard'
+
+const RoadmapPage = () => {
+    return (
+        <>
+            <NavabarRoadmap />
+            <RoadmapFeedCard />
+
+        </>
+    )
+}
+
+export default RoadmapPage
