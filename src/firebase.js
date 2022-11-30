d23c2ced7622b39f78ddb83de2738f05dbf0d098
Lena Sokolova <sokolhelena94@gmail.com>
2022-11-30 16:54:02 +0100

    refactor: change db config

--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,22 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
-import { collection } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCjVIkpex_q4jLyKqsKFkWd7gLVRzG24oI",
-    authDomain: "product-feedback-app-20ece.firebaseapp.com",
-    databaseURL: "https://product-feedback-app-20ece-default-rtdb.firebaseio.com",
-    projectId: "product-feedback-app-20ece",
-    storageBucket: "product-feedback-app-20ece.appspot.com",
-    messagingSenderId: "120698199881",
-    appId: "1:120698199881:web:4f603511fb6ea64db60498"
+    apiKey: "AIzaSyCXmxKzOTndx6qgMy26CdiangU0PPrkGc8",
+    authDomain: "product-feedback-app-64821.firebaseapp.com",
+    projectId: "product-feedback-app-64821",
+    storageBucket: "product-feedback-app-64821.appspot.com",
+    messagingSenderId: "600612784630",
+    appId: "1:600612784630:web:3c045cfa2691c889351fd7"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
-const feedbacksCollectionRef = collection(db, "feedbacks");
-const commentsCollectionRef = collection(db, "comments");
+// const feedbacksCollectionRef = collection(db, "feedbacks");
+// const commentsCollectionRef = collection(db, "comments");
 
 
-export { auth, provider, db, feedbacksCollectionRef, commentsCollectionRef };
+export { auth, provider, db };
