5b263780dedc9aaf089f92aee3f4e0ab6ea426b5
Lena Sokolova <sokolhelena94@gmail.com>
2022-12-01 12:12:20 +0100

    feat: create RoadmapPage

--- src/pages/RoadmapPage.js
+++ src/pages/RoadmapPage.js
@@ -1,15 +1,55 @@
 import React from 'react'
+import styled from 'styled-components';
+
 import NavabarRoadmap from '../components/NavabarRoadmap'
 import RoadmapFeedCard from '../components/RoadmapFeedCard'
 
 const RoadmapPage = () => {
     return (
-        <>
+        <RoadmapPageContainer>
             <NavabarRoadmap />
-            <RoadmapFeedCard />
+            <RoadmapCardsContainer>
+                <RoadmapCardsPlanned>
+                    <RoadmapFeedCard />
+                    <RoadmapFeedCard />
 
-        </>
+                </RoadmapCardsPlanned>
+                <RoadmapCardsInProgress>
+                    <RoadmapFeedCard />
+                    <RoadmapFeedCard />
+                    <RoadmapFeedCard />
+
+                </RoadmapCardsInProgress>
+                <RoadmapCardsLive>
+                    <RoadmapFeedCard />
+
+                </RoadmapCardsLive>
+
+            </RoadmapCardsContainer>
+
+        </RoadmapPageContainer>
     )
 }
 
-export default RoadmapPage
+export default RoadmapPage;
+
+
+const RoadmapPageContainer = styled.div`
+    max-width: 1000px;
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+    
+`;
+
+const RoadmapCardsContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+const RoadmapCardsPlanned = styled.div``;
+
+const RoadmapCardsInProgress = styled.div``;
+
+const RoadmapCardsLive = styled.div``;
+
