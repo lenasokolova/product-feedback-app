1221b0ac29d0653647ace76613259e4fcf11a517
Lena Sokolova <sokolhelena94@gmail.com>
2022-12-01 12:40:06 +0100

    refactor: add RoadmapInfo

--- src/pages/RoadmapPage.js
+++ src/pages/RoadmapPage.js
@@ -10,17 +10,32 @@ const RoadmapPage = () => {
             <NavabarRoadmap />
             <RoadmapCardsContainer>
                 <RoadmapCardsPlanned>
+                    <RoadmapCardsInfo>
+                        <h5>Planned (2)</h5>
+                        <h6>Ideas prioritized for research</h6>
+                    </RoadmapCardsInfo>
                     <RoadmapFeedCard />
                     <RoadmapFeedCard />
 
                 </RoadmapCardsPlanned>
                 <RoadmapCardsInProgress>
+                    <RoadmapCardsInfo>
+                        <h5>In-progress (3)</h5>
+                        <h6>Currently being developed</h6>
+
+                    </RoadmapCardsInfo>
+
                     <RoadmapFeedCard />
                     <RoadmapFeedCard />
                     <RoadmapFeedCard />
 
                 </RoadmapCardsInProgress>
                 <RoadmapCardsLive>
+                    <RoadmapCardsInfo>
+                        <h5>Live (1)</h5>
+                        <h6>Released features</h6>
+                    </RoadmapCardsInfo>
+
                     <RoadmapFeedCard />
 
                 </RoadmapCardsLive>
@@ -41,6 +56,21 @@ const RoadmapPageContainer = styled.div`
     margin: 0 auto;
     
 `;
+
+const RoadmapCardsInfo = styled.div`
+
+    margin: 20px 0px 20px;
+
+    >h5 {
+        font-size: 14px;
+    }
+
+    >h6 {
+        font-size: 12px;
+        font-weight: 500; 
+    }
+`;
+
 
 const RoadmapCardsContainer = styled.div`
     display: flex;
