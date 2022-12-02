1ebad63b9a009b2331ffcbb58bf1b2cb9ff12d5e
Lena Sokolova <sokolhelena94@gmail.com>
2022-12-02 15:56:56 +0100

    refactor: add rendering feedbacks logic as cards based on status

--- src/pages/RoadmapPage.js
+++ src/pages/RoadmapPage.js
@@ -1,42 +1,97 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components';
 
 import NavabarRoadmap from '../components/NavabarRoadmap'
 import RoadmapFeedCard from '../components/RoadmapFeedCard'
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { getFeedbacks } from './../redux/actions';
 
 const RoadmapPage = () => {
+
+    const navigate = useNavigate();
+
+    const { feedbacks } = useSelector(state => state.data)
+
+    const feedbacksPlanned = feedbacks.filter(function (feedback) {
+        return feedback.status === 'Planned'
+    }).map(({ id, status, title, category, detail, comments, upVotesCount }) => (
+        <RoadmapFeedCard
+            key={id}
+            onClick={() => navigate(`/feedback/${id}`)}
+            status={status}
+            title={title}
+            category={category}
+            detail={detail}
+            comments={comments?.length}
+            upVotesCount={upVotesCount?.length}
+        />
+    ))
+
+
+
+
+    const feedbacksInProgress = feedbacks.filter(function (feedback) {
+        return feedback.status === 'In-Progress'
+    }).map(({ id, status, title, category, detail, comments, upVotesCount }) => (
+        <RoadmapFeedCard
+            key={id}
+            onClick={() => navigate(`/feedback/${id}`)}
+            status={status}
+            title={title}
+            category={category}
+            detail={detail}
+            comments={comments?.length}
+            upVotesCount={upVotesCount?.length}
+        />
+    ))
+
+
+    const feedbacksLive = feedbacks.filter(function (feedback) {
+        return feedback.status === 'Live'
+    }).map(({ id, status, title, category, detail, comments, upVotesCount }) => (
+        <RoadmapFeedCard
+            key={id}
+            onClick={() => navigate(`/feedback/${id}`)}
+            status={status}
+            title={title}
+            category={category}
+            detail={detail}
+            comments={comments?.length}
+            upVotesCount={upVotesCount?.length}
+        />
+    ))
+
+
+
     return (
         <RoadmapPageContainer>
             <NavabarRoadmap />
             <RoadmapCardsContainer>
                 <RoadmapCardsPlanned>
                     <RoadmapCardsInfo>
-                        <h5>Planned (2)</h5>
+                        <h5>Planned ({feedbacksPlanned.length})</h5>
                         <h6>Ideas prioritized for research</h6>
                     </RoadmapCardsInfo>
-                    <RoadmapFeedCard />
-                    <RoadmapFeedCard />
+                    {feedbacksPlanned}
 
                 </RoadmapCardsPlanned>
                 <RoadmapCardsInProgress>
                     <RoadmapCardsInfo>
-                        <h5>In-progress (3)</h5>
+                        <h5>In-progress ({feedbacksInProgress.length})</h5>
                         <h6>Currently being developed</h6>
 
                     </RoadmapCardsInfo>
+                    {feedbacksInProgress}
 
-                    <RoadmapFeedCard />
-                    <RoadmapFeedCard />
-                    <RoadmapFeedCard />
 
                 </RoadmapCardsInProgress>
                 <RoadmapCardsLive>
                     <RoadmapCardsInfo>
-                        <h5>Live (1)</h5>
+                        <h5>Live ({feedbacksLive.length})</h5>
                         <h6>Released features</h6>
                     </RoadmapCardsInfo>
-
-                    <RoadmapFeedCard />
+                    {feedbacksLive}
 
                 </RoadmapCardsLive>
 
@@ -50,7 +105,7 @@ export default RoadmapPage;
 
 
 const RoadmapPageContainer = styled.div`
-    max-width: 1000px;
+    max-width: 800px;
     display: flex;
     flex-direction: column;
     margin: 0 auto;
@@ -58,7 +113,6 @@ const RoadmapPageContainer = styled.div`
 `;
 
 const RoadmapCardsInfo = styled.div`
-
     margin: 20px 0px 20px;
 
     >h5 {
