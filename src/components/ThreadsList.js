d4af300e9af0abac54fe7673b27f611c8c84f0a9
Lena Sokolova <sokolhelena94@gmail.com>
2022-12-02 15:53:11 +0100

    refactor: add delete feedback function and method to display feedbacks as a rendered ThreadExcerpt

--- src/components/ThreadsList.js
+++ src/components/ThreadsList.js
@@ -1,48 +1,54 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import ThreadExcerpt from './ThreadExcerpt';
 import { useSelector, useDispatch } from 'react-redux';
-import { deleteFeedback, getFeedbacks } from './../redux/actions';
+import { deleteFeedback, deleteFeedbackInit, getFeedbacks, getSingleFeedback } from './../redux/actions';
 import { Link, useNavigate } from 'react-router-dom';
+import { Timestamp } from 'firebase/firestore';
+import { nanoid } from '@reduxjs/toolkit';
+
+
 
 const ThreadsList = () => {
 
-    const { feedbacks: data } = useSelector(state => state.data);
-
-    let dispatch = useDispatch();
+    const dispatch = useDispatch();
     const navigate = useNavigate();
 
+    const { feedbacks } = useSelector(state => state.data)
 
     useEffect(() => {
-        dispatch(getFeedbacks());
-    }, [dispatch]);
+        dispatch(getFeedbacks())
 
-    const deletePost = async (id) => {
-        dispatch(deleteFeedback(id))
-    };
+    }, [])
 
-    const renderedFeedbacks = Object.keys(data).map((id) => {
-        return (
-            <div key={data[id].id} id={data[id].id}
 
-            >
-                <ThreadExcerpt
-                    onClick={() => navigate(`/feedback/${id}`)}
-                    title={data[id].title}
-                    category={data[id].category}
-                    detail={data[id].detail}
-                    commentsCount={data[id].commentsCount}
-                    upVotesCount={data[id].upVotesCount}
-                />
-                <DeleteBtn onClick={() => deletePost(data[id].id)}>Delete</DeleteBtn>
-                <Link to={`/update/${id}`}>Edit this feedback</Link>
-            </div>
-        )
-    })
+
+    const deleteFeedback = (id) => {
+        if (window.confirm("Are you sure younwant to delete this feedback?")) {
+            dispatch(deleteFeedbackInit(id));
+            console.log(id)
+        }
+    }
+
+
 
     return (
         <ThreadListContainer >
-            {renderedFeedbacks}
+            {feedbacks && feedbacks.map((item, index) => (
+                <div key={index}
+                >
+                    <ThreadExcerpt
+                        onClick={() => navigate(`/feedback/${item.id}`)}
+                        title={item.title}
+                        category={item.category}
+                        detail={item.detail}
+                        comments={item.comments?.length}
+                        upVotesCount={item.upVotesCount?.length}
+                    />
+                    <DeleteBtn onClick={() => deleteFeedback(item.id)}>Delete</DeleteBtn>
+                    <Link to={`/update/${item.id}`}>Edit this feedback</Link>
+                </div>
+            ))}
         </ThreadListContainer>
     )
 }
@@ -57,3 +63,73 @@ const ThreadListContainer = styled.div`
 const DeleteBtn = styled.button`
 
 `;
+
+
+
+
+
+
+
+// -----------------------------------------------
+// import React, { useState, useEffect } from 'react';
+// import styled from 'styled-components';
+// import ThreadExcerpt from './ThreadExcerpt';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { deleteFeedback, getFeedbacks } from './../redux/actions';
+// import { Link, useNavigate } from 'react-router-dom';
+
+// const ThreadsList = () => {
+
+//     const { feedbacks: data } = useSelector(state => state.data);
+
+//     const { title, category, detail, commentsCount, upVotesCount, id } = data
+//     console.log("this is data => ", data)
+
+//     let dispatch = useDispatch();
+//     const navigate = useNavigate();
+
+
+//     useEffect(() => {
+//         dispatch(getFeedbacks());
+//     }, [dispatch]);
+
+//     const deletePost = async (id) => {
+//         dispatch(deleteFeedback(id))
+//     };
+
+//     const renderedFeedbacks = Object.keys(data).map((id) => {
+//         return (
+//             <div key={data[id].id} id={data[id].id}
+
+//             >
+//                 <ThreadExcerpt
+//                     onClick={() => navigate(`/feedback/${data[id].id}`)}
+//                     title={data[id].title}
+//                     category={data[id].category}
+//                     detail={data[id].detail}
+//                     commentsCount={data[id].commentsCount}
+//                     upVotesCount={data[id].upVotesCount}
+//                 />
+//                 <DeleteBtn onClick={() => deletePost(id)}>Delete</DeleteBtn>
+//                 <Link to={`/update/${data[id].id}`}>Edit this feedback</Link>
+//             </div>
+//         )
+//     })
+
+//     return (
+//         <ThreadListContainer >
+//             {renderedFeedbacks}
+//         </ThreadListContainer>
+//     )
+// }
+
+// export default ThreadsList
+
+
+// const ThreadListContainer = styled.div`
+//     height: 100vh;
+// `;
+
+// const DeleteBtn = styled.button`
+
+// `;
