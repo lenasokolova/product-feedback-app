fd021d508004ff68775d9c6e278a9f5d1c3c6698
Lena Sokolova <sokolhelena94@gmail.com>
2022-12-06 16:31:28 +0100

    refactor: delete unused code

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import feedbackReducer from '../features/feedbacks/feedbackSlice';
-import userReducer from '../features/users/userSlice';
-
 
 export const store = configureStore({
   reducer: {
     feedback: feedbackReducer,
-    user: userReducer,
   },
 });
