6e7658b020dad2f108d9c82cd5e78c35fb029063
Lena Sokolova <sokolhelena94@gmail.com>
2023-02-21 20:17:36 +0100

    feat: create routes variables

---
+++ src/routes/routes.js
@@ -0,0 +1,12 @@
+const routes = {
+	REACT_APP_LOGIN_PATH: "/auth/login",
+	REACT_APP_REGISTER_PATH: "/auth/register",
+	REACT_APP_DASHBOARD_PATH: "/",
+	REACT_APP_ROADMAP_PATH: "/roadmap",
+	REACT_APP_SINGLE_FEEDBACK_PATH: "/feedback/",
+	REACT_APP_EDIT_FEEDBACK_PATH: "/update/",
+	REACT_APP_ADD_FEEDBACK_PATH: "/add-feedback",
+	REACT_APP_404_PATH: "*",
+};
+
+export default routes;
