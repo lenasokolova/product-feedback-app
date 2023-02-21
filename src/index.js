dcb7d48f02671b8be0e8124fabfe8c74defb8389
Lena Sokolova <sokolhelena94@gmail.com>
2023-02-21 20:13:14 +0100

    refactor: add GlobalCSS tag

--- src/index.js
+++ src/index.js
@@ -1,34 +1,21 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import { BrowserRouter } from "react-router-dom";
-import App from './App';
-import './index.css';
+import App from "./App";
+import GlobalCSS from "./global.css";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
-
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>
+	<React.StrictMode>
+		<GlobalCSS />
+		<BrowserRouter>
+			<Provider store={store}>
+				<App />
+			</Provider>
+		</BrowserRouter>
+	</React.StrictMode>
 );
-
-// const container = document.getElementById('root');
-// const root = createRoot(container);
-
-// root.render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <Provider store={store}>
-//         <App />
-//       </Provider>
-//     </BrowserRouter>
-//   </React.StrictMode>
-// );
