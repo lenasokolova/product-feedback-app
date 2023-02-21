f180f57feba4d013ce8616d515586fb25dcf25f2
Lena Sokolova <sokolhelena94@gmail.com>
2023-02-21 20:09:12 +0100

    refactor: remove unused files

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import feedbackReducer from '../features/feedbacks/feedbackSlice';
-
-export const store = configureStore({
-  reducer: {
-    feedback: feedbackReducer,
-  },
-});
